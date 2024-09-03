dd3adb18f58c7b5a7f42389c259d01a4235bbef2
lqjava <545209147@qq.com>
2024-09-03 12:04:27 +0800

    z

--- components/MarkdownTagP.js
+++ components/MarkdownTagP.js
@@ -8,7 +8,8 @@ const split_flag = "::";
 
 export default function PreviewsImage(props) {
     let {children} = props;
-    let isVideo = children && children.startsWith(video_flag);
+    console.log(children)
+    let isVideo = children && typeof children === 'string' && children.startsWith(video_flag);
     let [_,platforms,id] = isVideo ? children.split(split_flag) : [];
 
     console.log(_,platforms,id)
